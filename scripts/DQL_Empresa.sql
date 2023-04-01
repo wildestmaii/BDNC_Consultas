14006143941f8b16765d82ccdc6b78b815a1c712
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-04-01 00:00:56 -0300

    base para os nomes completos usando if

--- scripts/DQL_Empresa.sql
+++ scripts/DQL_Empresa.sql
@@ -1,38 +1,42 @@
 -- Responda com o comando SQL adequado para responder as seguintes consultas:
 
 -- 1) Listar nome e sobrenome ordenado por sobrenome.
-SELECT PrimeiroNome, UltimoNome
-	FROM Funcionarios ORDER BY SegundoNome;
+SELECT PrimeiroNome AS "Nome", UltimoNome AS "Sobrenome"
+	FROM Funcionarios 
+		ORDER BY SegundoNome;
 
 -- 2) Listar todos os campos de funcionários ordenados por cidade.
-SELECT * FROM Funcionarios ORDER BY Cidade;
+SELECT * FROM Funcionarios
+	ORDER BY Cidade;
 
 -- 3) Liste os funcionários que têm salário superior a R$ 1.000,00 ordenados pelo nome completo
-SELECT PrimeiroNome, SegundoNome, UltimoNome, Salario, Funcao
-	FROM funcionarios WHERE Salario > 1000 ORDER BY PrimeiroNome, SegundoNome, UltimoNome;
+SELECT IF(SegundoNome IS NOT NULL, CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome), CONCAT(PrimeiroNome, " ", UltimoNome)) AS "Nome completo", CONCAT("R$ ", Salario) AS "Salário", Funcao AS "Função"
+	FROM Funcionarios WHERE Salario > 1000 
+		ORDER BY "Nome completo";
 
+-- LEMBRAR DE RODAR O IF -------------------------------------------------------------------------------------------------------------------------------
 -- 4) Liste a data de nascimento e o primeiro nome dos funcionários ordenados do mais novo para o mais velho
-SELECT DataNasci, PrimeiroNome, SegundoNome, UltimoNome
+SELECT DataNasci AS "Data de nascimento", CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome) AS "Nome completo"
 	FROM Funcionarios 
 		ORDER BY DataNasci DESC;
-
--- 5) Liste os funcionários como uma lista telefônica, com enedeço e cidade
-SELECT PrimeiroNome, SegundoNome, UltimoNome, Fone, Endereco, Cidade, CEP
-	FROM Funcionarios 
+-- LEMBRAR DE RODAR O IF -------------------------------------------------------------------------------------------------------------------------------
+-- 5) Liste os funcionários como uma lista telefônica, com endereço e cidade
+SELECT CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome) AS "Nome completo", Fone, Endereco AS "Endereço", Cidade, CEP
+	FROM Funcionarios
 		ORDER BY PrimeiroNome, SegundoNome, UltimoNome;
-
+-- LEMBRAR DE RODAR O IF -------------------------------------------------------------------------------------------------------------------------------
 -- 6) Liste o nome completo de todos os funcionários que não tenham segundo nome
-SELECT PrimeiroNome, UltimoNome
+SELECT CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome) AS "Nome completo"
 	FROM Funcionarios
 		WHERE SegundoNome IS NULL;
-
+-- LEMBRAR DE RODAR O IF -------------------------------------------------------------------------------------------------------------------------------
 -- 7) Liste os nomes dos funcionários que moram em Ituiutaba e que exerçam a função de Telefonista
-SELECT PrimeiroNome, SegundoNome, UltimoNome 
+SELECT CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome) AS 'Nomes'
 	FROM Funcionarios 
 		WHERE Cidade = 'Itaiutaba' AND Funcao = 'Telefonista';
-
+-- LEMBRAR DE RODAR O IF -------------------------------------------------------------------------------------------------------------------------------
 -- 8) Liste os nomes dos funcionários que trabalham no departamento 2
-SELECT PrimeiroNome, SegundoNome, UltimoNome 
+SELECT CONCAT(PrimeiroNome, " ", SegundoNome, " ", UltimoNome) AS "Nome completo"
 	FROM Funcionarios WHERE CodigoDepartamento = 2;
 
 -- 9) Liste o nome e o código do departamento de todos os funcionários que ganham mais do que R$ 2000,00
