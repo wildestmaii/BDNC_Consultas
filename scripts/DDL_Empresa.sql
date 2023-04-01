879a6b59794184e81000a1f27fa39d7e2fc006bc
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-03-31 23:26:04 -0300

    ajustes no ddl

--- scripts/DDL_Empresa.sql
+++ scripts/DDL_Empresa.sql
@@ -1,19 +1,18 @@
-create database Empresas;
-USE Empresas;
+CREATE DATABASE `Empresas`;
+USE `Empresas`;
 
-
-CREATE TABLE Funcionarios (
-codigo int primary key,
-primeiroNome varchar(20),
-segundoNome varchar(20),
-ultimoNome varchar(20),
-dataNasci date,
-CPF numeric(11),
-RG numeric(9),
-endereco varchar(40),
-CEP numeric(9),
-cidade varchar(40),
-fone numeric(13),
-codigoDepartamento int,
-funcao varchar(20),
-salario numeric(10,2));
+CREATE TABLE `Funcionarios` (
+`Codigo` INT PRIMARY KEY,
+`PrimeiroNome` VARCHAR(30),
+`SegundoNome` VARCHAR(30),
+`UltimoNome` VARCHAR(30),
+`DataNasci` DATE,
+`CPF` NUMERIC(11),
+`RG` NUMERIC(9),
+`Endereco` VARCHAR(40),
+`CEP` NUMERIC(8),
+`Cidade` VARCHAR(40),
+`Fone` NUMERIC(14),
+`CodigoDepartamento` INT,
+`Funcao` VARCHAR(20),
+`Salario` NUMERIC(10,2));
